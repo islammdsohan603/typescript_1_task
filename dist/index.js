bd83a00cb430e957b49a37a17c3b4372b69a2010
islammdsohan603 <islammdsohan603@gmail.com>
2026-06-12 15:29:30 +0600

    tupple typescript

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let scores = [85, 98, 58];
-let names = ['John', 'Jane', 'Jim'];
-let scores2 = [85, 98, 58];
-let names2 = ['John', 'Jane', 'Jim'];
-let scores3 = [85, 98, 58];
-let names3 = ['John', 'Jane', 'Jim'];
-let scores4 = [85, 98, 58];
-let names4 = ['John', 'Jane', 'Jim'];
-console.log(scores[0]);
-console.log(names[0]);
-console.log(scores2[0]);
-console.log(names2[0]);
-console.log(scores3[0]);
-console.log(names3[0]);
-console.log(scores4[0]);
-console.log(names4[0]);
+let students = ['sohan', 20, 3.2];
+console.log(students);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,2 @@
-let scores: number[] = [85, 98, 58];
-
-let names: string[] = ['John', 'Jane', 'Jim'];
-
-let scores2: Array<number> = [85, 98, 58];
-let names2: Array<string> = ['John', 'Jane', 'Jim'];
-
-let scores3: number[] = [85, 98, 58];
-let names3: string[] = ['John', 'Jane', 'Jim'];
-
-let scores4: number[] = [85, 98, 58];
-let names4: string[] = ['John', 'Jane', 'Jim'];
-
-console.log(scores[0]);
-console.log(names[0]);
-console.log(scores2[0]);
-console.log(names2[0]);
-console.log(scores3[0]);
-console.log(names3[0]);
-console.log(scores4[0]);
-console.log(names4[0]);
+let students: [string, number, number] = ['sohan', 20, 3.2];
+console.log(students);
