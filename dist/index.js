f2a041fc70c690571ef997cf7d2dd4d6ad293c6e
islammdsohan603 <islammdsohan603@gmail.com>
2026-06-12 15:22:59 +0600

    typescript array

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-for (let i = 0; i <= 100; i++) {
-    if (i % 3 === 0) {
-        console.log(`${i} is a multiple of 3`);
-    }
-    if (i % 2 === 0) {
-        console.log(`${i} is a multiple of 2`);
-    }
-}
+let scores = [85, 98, 58];
+let names = ['John', 'Jane', 'Jim'];
+let scores2 = [85, 98, 58];
+let names2 = ['John', 'Jane', 'Jim'];
+let scores3 = [85, 98, 58];
+let names3 = ['John', 'Jane', 'Jim'];
+let scores4 = [85, 98, 58];
+let names4 = ['John', 'Jane', 'Jim'];
+console.log(scores[0]);
+console.log(names[0]);
+console.log(scores2[0]);
+console.log(names2[0]);
+console.log(scores3[0]);
+console.log(names3[0]);
+console.log(scores4[0]);
+console.log(names4[0]);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-for (let i: number = 0; i <= 100; i++) {
-  if (i % 3 === 0) {
-    console.log(`${i} is a multiple of 3`);
-  }
-  if (i % 2 === 0) {
-    console.log(`${i} is a multiple of 2`);
-  }
-}
+let scores: number[] = [85, 98, 58];
+
+let names: string[] = ['John', 'Jane', 'Jim'];
+
+let scores2: Array<number> = [85, 98, 58];
+let names2: Array<string> = ['John', 'Jane', 'Jim'];
+
+let scores3: number[] = [85, 98, 58];
+let names3: string[] = ['John', 'Jane', 'Jim'];
+
+let scores4: number[] = [85, 98, 58];
+let names4: string[] = ['John', 'Jane', 'Jim'];
+
+console.log(scores[0]);
+console.log(names[0]);
+console.log(scores2[0]);
+console.log(names2[0]);
+console.log(scores3[0]);
+console.log(names3[0]);
+console.log(scores4[0]);
+console.log(names4[0]);
