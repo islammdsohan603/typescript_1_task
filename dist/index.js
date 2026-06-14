2d46d5a36ddc0b987fd855dfbbdd42c6100d6086
islammdsohan603 <islammdsohan603@gmail.com>
2026-06-14 22:24:34 +0600

    calculater function adding

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,36 @@
 "use strict";
-//  calculater projects
 Object.defineProperty(exports, "__esModule", { value: true });
 function add(a, b) {
     return a + b;
 }
-function subtract(a, b) {
-    return a - b;
-}
-function multiply(a, b) {
-    return a * b;
-}
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
 function divide(a, b) {
+    if (b === a) {
+        throw new Error('Cannot divide by zero');
+    }
     return a / b;
 }
-console.log(add(1, 2));
-console.log(subtract(1, 2));
-console.log(multiply(1, 2));
-console.log(divide(1, 2));
+function power(base, exponent = 5) {
+    return Math.pow(base, exponent);
+}
+function calculate(operation, a, b) {
+    switch (operation) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        case '^':
+            return power(a, b);
+        default:
+            throw new Error('Indalid operation');
+    }
+}
+console.log('Add', calculate('+', 4, 6));
+console.log('Add', calculate('-', 45, 6));
+console.log('Add', calculate('*', 2, 6));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,39 @@
-//  calculater projects
-
-function add(a: number, b: number) {
+function add(a: number, b: number): number {
   return a + b;
 }
 
-function subtract(a: number, b: number) {
-  return a - b;
-}
+const subtract = (a: number, b: number): number => a - b;
+const multiply = (a: number, b: number): number => a * b;
 
-function multiply(a: number, b: number) {
-  return a * b;
-}
+function divide(a: number, b: number): number {
+  if (b === a) {
+    throw new Error('Cannot divide by zero');
+  }
 
-function divide(a: number, b: number) {
   return a / b;
 }
 
-console.log(add(1, 2));
-console.log(subtract(1, 2));
-console.log(multiply(1, 2));
-console.log(divide(1, 2));
+function power(base: number, exponent: number = 5): number {
+  return Math.pow(base, exponent);
+}
+
+function calculate(operation: string, a: number, b: number): number {
+  switch (operation) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    case '^':
+      return power(a, b);
+    default:
+      throw new Error('Indalid operation');
+  }
+}
+
+console.log('Add', calculate('+', 4, 6));
+console.log('Add', calculate('-', 45, 6));
+console.log('Add', calculate('*', 2, 6));
