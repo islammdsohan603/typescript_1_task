c33bbdf399954164f6d0daf861d044dbf06435aa
islammdsohan603 <islammdsohan603@gmail.com>
2026-06-15 08:17:23 +0600

    objects function

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function add(a, b) {
-    return a + b;
+const student = {
+    name: 'sohan',
+    age: 20,
+};
+function printUser(user) {
+    console.log(`${user.name} and age is ${user.age} years old`);
 }
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-function divide(a, b) {
-    if (b === a) {
-        throw new Error('Cannot divide by zero');
-    }
-    return a / b;
-}
-function power(base, exponent = 5) {
-    return Math.pow(base, exponent);
-}
-function calculate(operation, a, b) {
-    switch (operation) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case '*':
-            return multiply(a, b);
-        case '/':
-            return divide(a, b);
-        case '^':
-            return power(a, b);
-        default:
-            throw new Error('Indalid operation');
-    }
-}
-console.log('Add', calculate('+', 4, 6));
-console.log('Add', calculate('-', 45, 6));
-console.log('Add', calculate('*', 2, 6));
+printUser(student);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,10 @@
-function add(a: number, b: number): number {
-  return a + b;
+const student = {
+  name: 'sohan',
+  age: 20,
+};
+
+function printUser(user: { name: string; age: number }) {
+  console.log(`${user.name} and age is ${user.age} years old`);
 }
 
-const subtract = (a: number, b: number): number => a - b;
-const multiply = (a: number, b: number): number => a * b;
-
-function divide(a: number, b: number): number {
-  if (b === a) {
-    throw new Error('Cannot divide by zero');
-  }
-
-  return a / b;
-}
-
-function power(base: number, exponent: number = 5): number {
-  return Math.pow(base, exponent);
-}
-
-function calculate(operation: string, a: number, b: number): number {
-  switch (operation) {
-    case '+':
-      return add(a, b);
-    case '-':
-      return subtract(a, b);
-    case '*':
-      return multiply(a, b);
-    case '/':
-      return divide(a, b);
-    case '^':
-      return power(a, b);
-    default:
-      throw new Error('Indalid operation');
-  }
-}
-
-console.log('Add', calculate('+', 4, 6));
-console.log('Add', calculate('-', 45, 6));
-console.log('Add', calculate('*', 2, 6));
+printUser(student);
